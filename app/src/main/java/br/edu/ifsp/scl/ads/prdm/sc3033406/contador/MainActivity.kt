993ad7ae6bde0a6cda2cba8d8890ce61e6eb578d
Vinicius64 <oliveira.vinicius64@gmail.com>
2025-08-22 21:45:54 -0300

    feat(MainActivity): implements counter with initial count input

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033406/contador/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033406/contador/MainActivity.kt
@@ -1,20 +1,31 @@
 package br.edu.ifsp.scl.ads.prdm.sc3033406.contador
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var cliqueBt: Button
+    private lateinit var contadorTv: TextView
+    private lateinit var inicialEt: EditText
+    private var contador: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        cliqueBt = findViewById(R.id.clique_bt)
+        inicialEt = findViewById(R.id.inicial_et)
+        contadorTv = findViewById(R.id.contador_tv)
+        cliqueBt.setOnClickListener {
+            contadorTv.text = (++contador).toString()
+        }
+
+        inicialEt.addTextChangedListener { p0 ->
+            contador = p0.toString().toInt()
         }
     }
 }
